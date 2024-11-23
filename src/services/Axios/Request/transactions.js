ba4faa0c97558e70fc70da27e944d0fcc33c6f6a
MostafaMemari <m.memari7@yahoo.com>
2024-11-23 14:48:23 +0330

    Removed all files

--- src/services/Axios/Request/transactions.js
+++
@@ -1,8 +0,0 @@
-import httpService from '../Configs/httpService';
-
-export const transactionsProductService = (productId, data) => {
-  return httpService(`/transactions/product/${productId}`, 'post', data);
-};
-export const getTransactionsProductService = (page, limit) => {
-  return httpService(`/transactions?page=${page}&limit=${limit}`, 'get');
-};
